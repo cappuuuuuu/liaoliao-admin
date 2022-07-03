f39080e7a08f5fc06c1efa5d4d091be4df3ac30c
cappuuuuuu <w1814946281@gmail.com>
2022-07-03 16:33:48 +0800

    feat: initialize vuesax loading component & apply loading effect when executing HTTP request

--- src/library/vuesax.js
+++ src/library/vuesax.js
@@ -8,19 +8,35 @@ function iconClass (color) {
   return `<i class='${className[color]}' ></i>`
 }
 
+const defaultConfig = {
+  notify (options) {
+    return {
+      position: 'top-right',
+      icon: iconClass(options.color),
+      flat: true
+    }
+  },
+  loading: {
+    type: 'points',
+    color: '#fff',
+    background: 'rgba(0, 0, 0, .85)'
+  }
+}
+
 export function initialVueSax (vuesax) {
   Vue.prototype.$vs = vuesax
 }
 
 export function notify (options) {
-  const defaultConfig = {
-    position: 'top-right',
-    icon: iconClass(options.color),
-    flat: true
-  }
-
   return Vue.prototype.$vs.notification({
-    ...defaultConfig,
+    ...defaultConfig.notify(options),
+    ...options
+  })
+}
+
+export function loading (options) {
+  return Vue.prototype.$vs.loading({
+    ...defaultConfig.loading,
     ...options
   })
 }
--- src/services/xhr.js
+++ src/services/xhr.js
@@ -1,14 +1,14 @@
-import { notify } from '@/library/vuesax'
+import { notify, loading } from '@/library/vuesax'
 import router from '@/router'
 import axios from 'axios'
 import { timeout } from '@/utils'
-import CONSTANT_MESSAGE from '@/constants/message'
+import MESSAGE from '@/constants/message'
 const { VUE_APP_SERVER_ORIGIN: SERVER_ORIGIN } = process.env
 
 const errorHandle = {
   // 401: access_token is not valid, router will redirect to login page
   401: async function () {
-    notify({ text: CONSTANT_MESSAGE.notification.error.RELOGIN, color: 'danger' })
+    notify({ text: MESSAGE.notification.error.RELOGIN, color: 'danger' })
 
     await timeout(1000)
     router.push('/')
@@ -19,7 +19,8 @@ export default function xhr ({
   method = 'get',
   url = '/',
   data = null,
-  params = null
+  params = null,
+  applyLoading = true
 }) {
   const options = {
     method,
@@ -30,12 +31,14 @@ export default function xhr ({
   }
 
   return new Promise((resolve, reject) => {
+    const isLoading = applyLoading && loading()
+
     axios(options)
       .then(res => {
         resolve(res.data)
       })
       .catch(async err => {
-        const errorMessage = err.response.data?.message ?? CONSTANT_MESSAGE.api.error.UNKNOWN
+        const errorMessage = err.response.data?.message ?? MESSAGE.api.error.UNKNOWN
         const errorStatus = err.response.status
 
         errorHandle[errorStatus]
@@ -44,5 +47,6 @@ export default function xhr ({
 
         reject(err)
       })
+      .finally(() => applyLoading && isLoading.close())
   })
 }
