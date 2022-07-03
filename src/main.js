b965d20ff79495939b582cbf49bb3f0f199260a0
cappuuuuuu <w1814946281@gmail.com>
2022-07-03 12:47:12 +0800

    chore: add UI-library: vuesax, and remove element-ui

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-// ElementUI
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
 // Vee-Validate
 import { ValidationProvider } from 'vee-validate'
@@ -13,18 +10,21 @@ import './validator'
 // Pinia
 import { createPinia, PiniaVuePlugin } from 'pinia'
 
+// Vuesax
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' // Vuesax styles
+import 'boxicons/css/boxicons.min.css'// Vuesax Icon use boxicons
+
 // Tailwind CSS
 import './index.css'
 
-Vue.use(ElementUI)
 Vue.use(PiniaVuePlugin)
+Vue.use(Vuesax)
 Vue.component('ValidationProvider', ValidationProvider)
 Vue.config.productionTip = false
 
-const pinia = createPinia()
-
 new Vue({
   router,
-  pinia,
+  pinia: createPinia(),
   render: h => h(App)
 }).$mount('#app')
