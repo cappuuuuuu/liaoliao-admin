2ef0411ec5b2900c677b8df22f6b3777696a1cd5
cappuuuuuu <w1814946281@gmail.com>
2022-07-05 03:06:36 +0800

    feat: add tailwind config to store

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,20 @@
 <script>
 import { mapActions } from 'pinia'
 import { useThemeStore } from '@/stores/theme'
+import { useTailwindStyleStore } from '@/stores/tailwind'
 import { initialVueSax } from '@/library/vuesax'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../tailwind.config.js'
 
 export default {
   created () {
     initialVueSax(this.$vs)
     this.initializeTheme()
+    this.setTailwindConfig(resolveConfig(tailwindConfig))
   },
   methods: {
-    ...mapActions(useThemeStore, ['initializeTheme'])
+    ...mapActions(useThemeStore, ['initializeTheme']),
+    ...mapActions(useTailwindStyleStore, { setTailwindConfig: 'setConfig' })
   }
 }
 </script>
